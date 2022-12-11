3cb79e92db18093717d307b1e0b5549475b3fc5a
Keisuke Takagi <mit@takagi.app>
2022-12-12 06:16:46 +0900

    feat: MainActivityを実装した

--- app/src/main/java/com/takagimeow/adaptiveuisample/MainActivity.kt
+++ app/src/main/java/com/takagimeow/adaptiveuisample/MainActivity.kt
@@ -1,29 +1,63 @@
 package com.takagimeow.adaptiveuisample
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.annotation.RequiresApi
 import androidx.compose.material3.Text
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.window.layout.FoldingFeature
+import androidx.window.layout.WindowInfoTracker
 import com.takagimeow.adaptiveuisample.ui.theme.AdaptiveUISampleTheme
+import com.takagimeow.adaptiveuisample.ui.utils.DevicePosture
+import com.takagimeow.adaptiveuisample.ui.utils.isBookPosture
+import com.takagimeow.adaptiveuisample.ui.utils.isSeparating
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // 折りたたみの状態を確認し、かつモニタリングする
+        val devicePostureFlow = WindowInfoTracker.getOrCreate(this)
+            .windowLayoutInfo(this)
+            .flowWithLifecycle(this.lifecycle) // lifecycle-viewmodel-compose:2.5.1以降対応
+            .map { layoutInfo ->
+                val foldingFeature =
+                    layoutInfo.displayFeatures
+                        .filterIsInstance<FoldingFeature>()
+                        .firstOrNull()
+                when {
+                    isBookPosture(foldingFeature) ->
+                        DevicePosture.BookPosture(foldingFeature.bounds)
+                    isSeparating(foldingFeature) ->
+                        DevicePosture.Separating(foldingFeature.bounds, foldingFeature.orientation)
+                    else -> DevicePosture.NormalPosture
+                }
+            }
+            .stateIn(
+                scope = lifecycleScope,
+                started = SharingStarted.Eagerly,
+                initialValue = DevicePosture.NormalPosture
+            )
+
         setContent {
             AdaptiveUISampleTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                val windowSize = calculateWindowSizeClass(this)
+                val devicePosture = devicePostureFlow.collectAsState().value
+
+                AdaptiveUISampleApp(windowSize = windowSize.widthSizeClass, foldingDevicePosture = devicePosture)
             }
         }
     }
